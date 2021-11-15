b2fccac17c8778f785b3d0385aacb8bac99c180f
Valeria Sánchez <90358728+valesor@users.noreply.github.com>
2021-11-15 16:52:57 -0300

    Add files via upload

---
+++ js/jsproyecto.js
@@ -0,0 +1,19 @@
+
+const input = document.querySelectorAll(".inp")
+
+const inputFirst = document.querySelector(".inp-character-1")
+const inputsSecond = document.querySelector(".inp-character-2")
+const inputThird = document.querySelector(".inp-character-3")
+const inputForth = document.querySelector(".inp-letters")
+const inputFive = document.querySelector(".inp-read")
+const inputSix = document.querySelector(".inp-all")
+const inputSeven = document.querySelector(".inp-letters-1 ")
+const inputEight = document.querySelector(".inp-read-1")
+const inputNine = document.querySelector(".inp-all-character-1")
+const inputTen = document.querySelector(".inp-all-character-2")
+
+input.addEventListener ("click", funcion()) {
+    
+}
+
+
